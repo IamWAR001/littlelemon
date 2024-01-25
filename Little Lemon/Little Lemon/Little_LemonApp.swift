d0269cfc68bf96d8885543bc2687782c69acee75
walter-fl <40576833+IamWAR8@users.noreply.github.com>
2024-01-25 01:07:04 -0400

    project created

---
+++ Little Lemon/Little Lemon/Little_LemonApp.swift
@@ -0,0 +1,17 @@
+//
+//  Little_LemonApp.swift
+//  Little Lemon
+//
+//  Created by Walter A Ramirez on 1/25/24.
+//
+
+import SwiftUI
+
+@main
+struct Little_LemonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
